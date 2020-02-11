58d2f12b58b8413a73cca79a03b786a71d76b4e6
dharmendra-sehgal <dharmendra.sehgal@decorist.com>
2020-02-11 15:48:51 +0530

    Added nodemon for hotreloading and api error handling

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
+//routes sould server here
 app.use('/products',productRoutes);
 app.use('/orders',orderRoutes);
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        err: {
+           message: err.message
+        }
+    });
+});
+
 
 module.exports = app;
